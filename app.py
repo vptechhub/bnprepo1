61ba823d0f3816d7163b7cdf3cef0c539724531f
vptechhub <vptechhub@outlook.com>
2026-09-04 12:55:45 +0530

    C3

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Sample FlaskApp Containerization demo----new version1"
+    return "<h1>Sample FlaskApp Containerization demo----new version"
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080)
